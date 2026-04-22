fd74c379ae89dac5e7f375464122882e8d3bd188
destinyrhemabitrus <destinyrhemabitrus@gmail.com>
2026-04-22 10:16:54 +0100

    Add files via upload

---
+++ tears of joy/fizzbuzz.py
@@ -0,0 +1,13 @@
+for count in range(1, 51, 1):
+    if count%5 == 0 and count%3 == 0:
+        print("fizzbuzz")
+    elif count%5 == 0:
+        print("buzz")
+    elif count%3 == 0:
+        print("fizz")
+    else:
+        print(count)
+
+
+
+    
